4e5e51299edf0535f0f59fb085378c88acb5cdfb
tanvir <tanvirislam173389@gmail.com>
2026-06-14 19:26:05 +0600

    added 7th query

--- query.sql
+++ query.sql
@@ -82,3 +82,15 @@ ON m.match_id = b.match_id
 SELECT u.user_id, u.full_name, b.booking_id
 FROM Users AS u LEFT JOIN Bookings AS b
 ON u.user_id = b.user_id
+
+-- query 6
+SELECT booking_id, match_id, total_cost
+FROM Bookings 
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings)
+
+-- query 7
+SELECT match_id, fixture, base_ticket_price
+FROM Matches 
+ORDER BY base_ticket_price DESC
+OFFSET 1
+LIMIT 2
